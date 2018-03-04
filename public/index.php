474166a7da4537d8709135222ee943de822c3d9a
karolinaszpera <karolinaszpera6@gmail.com>
2018-03-04 11:37:05 +0000

    HTML - fix

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Karolina Szpera {%if page.title %} - {{ page.title }}{% endif %}</title>
+    <title>Karolina Szpera</title>
     <meta name="description" content="">
     <meta name="author" content="Karolina Szpera">
 
