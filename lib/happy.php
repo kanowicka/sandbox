a8596934975737e2b471123273b03ac29694126b
karolinaszpera <karolinaszpera6@gmail.com>
2018-03-04 18:01:38 +0000

    POC happy numbers

--- lib/happy.php
+++ lib/happy.php
@@ -1,5 +1,27 @@
 <?php
 
+$x = 19;
+
+$y = $x;
+
+while ($y != 1 && $y != 4) {
+    $z = 0;
+
+    echo $y . PHP_EOL;
+
+    foreach(str_split($y) as $n) {
+        $z = $z + pow($n, 2);
+    }
+
+    $y = $z;
+}
+
+echo 'Result: ' . $y;
+
+// var_dump($y, $z);
+die();
+
+
 function isHappyNumber($num)
 {
     return true;
