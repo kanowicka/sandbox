e2b05c0785bb249c126fead7338c842a9754b505
karolinaszpera <karolinaszpera6@gmail.com>
2018-03-04 17:42:56 +0000

    Front

---
+++ lib/happy.php
@@ -0,0 +1,6 @@
+<?php
+
+function isHappyNumber($num)
+{
+    return true;
+}
--- public/index.php
+++ public/index.php
@@ -1,8 +1,17 @@
 <?php
-                                
-    $result = $_POST['karola'];
-    $result = str_replace(' ', '', $result);
-    $result = strtoupper($result);
+
+include('../lib/happy.php');
+
+if (isset($_POST['number']) && !empty($_POST['number'])) {
+    $number = $_POST['number'];
+
+    if (!is_numeric($number)) {
+        die('Podaj liczbe');
+    }
+
+    $isHappy = isHappyNumber($number);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,15 +52,29 @@
             <!-- CONTENT START -->
             <h2>Formularz</h2>
 
-            <form method="post">
-                <textarea name="karola" class="form-control" rows="3"></textarea>
-                <button type="submit" class="btn btn-default">Wyslij</button>
+            <form class="form-horizontal" method="post">
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Liczba</label>
+                <div class="col-sm-10">
+                  <input class="form-control" id="inputEmail3" placeholder="0" name="number">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default">Sprawdz</button>
+                </div>
+              </div>
             </form>
-
-            <h2>Wynik</h2>
-
-            <div class="well"><?=$result?></div>
-
+            <?php
+                if (isset($number)) {
+                    echo ('<h2>Wynik</h2>');
+                    if ($isHappy == true) {
+                        echo ('<div class="alert alert-success" role="alert">' . $number . ' jest szczęśliwa!</div>');
+                    } else {
+                        echo ('<div class="alert alert-danger" role="alert">Niestety ' . $number . ' nie jest.</div>');
+                    }
+                }
+            ?>
             <!-- CONTENT STOP -->
         </div>
       <footer class="footer">
