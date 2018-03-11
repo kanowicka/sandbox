2761038519b545870a0f2717f76a2201363a9dd8
karolinaszpera <karolinaszpera6@gmail.com>
2018-03-11 13:03:57 +0000

    user form

--- public/user.php
+++ public/user.php
@@ -2,7 +2,19 @@
     include('../lib/main.php');
     include('../templates/top.php');
 ?>
-
+<form class="form-horizontal" method="get">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Podaj swoje ID</label>
+    <div class="col-sm-10">
+      <input type="numbers" class="form-control" id="inputEmail3" placeholder="ID" name="id">
+    </div>
+  </div>
+  <div class="form-group">
+   <div class="col-sm-offset-2 col-sm-10">
+     <button type="submit" class="btn btn-default">Pokaż użytkownika</button>
+   </div>
+ </div>
+</form>
 
 <?php
     include('../templates/bottom.php');
