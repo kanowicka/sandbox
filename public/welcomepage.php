0a81c48c7b64f909f9b67bf9b9d0cbfb74da6b4e
karolinaszpera <karolinaszpera6@gmail.com>
2018-03-11 11:52:09 +0000

    isset

--- public/welcomepage.php
+++ public/welcomepage.php
@@ -1,6 +1,22 @@
 <?php
     include('../lib/main.php');
 
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
+        // 1
+    } else {
+        throw new Exception("Podaj imię");
+    }
+    if (isset($_POST['age']) && !empty($_POST['age'])){
+
+    } else {
+        throw new Exception("Podaj wiek");
+    }
+    if (isset($_POST['gender']) && !empty($_POST['gender'])){
+
+    } else {
+        throw new Exception("Podaj płeć");
+    }
+
     $id = saveNewUser($_POST['name'], $_POST['age'], $_POST['gender']);
 
     include('../templates/top.php');
