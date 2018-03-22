73314b344666be1c3aa07406fa659134cb435061
karolinaszpera <karolinaszpera6@gmail.com>
2018-03-22 21:37:00 +0000

    function gcd

--- lib/math.php
+++ lib/math.php
@@ -8,5 +8,16 @@
  */
 function gcd($a, $b)
 {
+    return ($a % $b) ? gcd($b, $a % $b) : $b;
+}
 
+function gcd2($a, $b)
+{
+    $r = $a % $b;
+
+    if ($r != 0) {
+        return gcd2($b, $r);
+    } else {
+        return $b;
+    }
 }
